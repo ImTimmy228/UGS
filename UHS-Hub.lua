e3d95f4e31115cc7dbb15f5ce095b82f8d317d5f
CodeCracker <74459442+ImTimmy228@users.noreply.github.com>
2024-06-01 14:38:16 +0500

    Update UHS-Hub.lua

--- UHS-Hub.lua
+++ UHS-Hub.lua
@@ -1,1 +1,14 @@
+-- getgenv().SkipHub = false -- if TRUE then automatically load game script.
 
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))() -- UI Library
+
+getgenv().SecureMode = true -- Worse animations, better performance.
+
+local Window = Rayfield:CreateWindow({ -- UI Window, contains tabs
+   Name = "UHS Hub",
+   LoadingTitle = "UHS Hub",
+   LoadingSubtitle = "Open Source Hub"
+})
+local GamesTab = Window:CreateTab("Games", 4483362458) -- UI Tabs, contain elements
+local ScriptsTab = Window:CreateTab("Scripts", 4483362458) -- UI Tabs, contain elements
+local PlayerTab = Window:CreateTab("Player", 4483362458) -- UI Tabs, contain elements
